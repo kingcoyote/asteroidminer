72cb247843dd8a589623e983bf781df1f62c9e44
Steve Phillips <kingcoyote85@gmail.com>
2015-11-06 16:31:06 -0800

    got the minerals working again

--- Assets/Scripts/AsteroidScript.cs
+++ Assets/Scripts/AsteroidScript.cs
@@ -26,9 +26,15 @@ public class AsteroidScript : MonoBehaviour {
 
     private void DestroyAsteroid()
     {
-        foreach (var mineral in Minerals)
+        var rigidbody2d = gameObject.GetComponent<Rigidbody2D>();
+        var mass = rigidbody2d.mass;
+        var mineralCount = (int)(Random.Range(0, mass) / 100);
+
+        for (int i = 0; i < mineralCount; i++)
         {
-            
+            var mineral = Instantiate(Minerals[Random.Range(0, Minerals.Count - 1)]);
+            mineral.position = transform.position + new Vector3(Random.Range(0f, 0.5f), Random.Range(0f, 0.5f), 0);
+            mineral.GetComponent<Rigidbody2D>().velocity = rigidbody2d.velocity + new Vector2(Random.Range(-0.5f, 0f), Random.Range(-0.5f, 0.5f));
         }
 
         Destroy(gameObject);
