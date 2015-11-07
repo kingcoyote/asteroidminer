4e2e960e3d659c38d7e02847d8cce6d426fe58de
Steve Phillips <kingcoyote85@gmail.com>
2015-11-07 10:04:57 -0800

    added code so that asteroids can fragment into subasteroids on death

--- Assets/Scripts/AsteroidScript.cs
+++ Assets/Scripts/AsteroidScript.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 public class AsteroidScript : MonoBehaviour {
 
     public List<Transform> Minerals;
+    public List<Transform> SubAsteroids;
     public int Health;
 
     // Use this for initialization
@@ -26,15 +27,22 @@ public class AsteroidScript : MonoBehaviour {
 
     private void DestroyAsteroid()
     {
-        var rigidbody2d = gameObject.GetComponent<Rigidbody2D>();
-        var mass = rigidbody2d.mass;
-        var mineralCount = (int)(Random.Range(0, mass) / 100);
+        var rb = gameObject.GetComponent<Rigidbody2D>();
+        var mass = rb.mass;
+        var mineralCount = (int)(Random.Range(mass/4, mass) / 100);
 
-        for (int i = 0; i < mineralCount; i++)
+        for (var i = 0; i < mineralCount; i++)
         {
             var mineral = Instantiate(Minerals[Random.Range(0, Minerals.Count - 1)]);
             mineral.position = transform.position + new Vector3(Random.Range(0f, 0.5f), Random.Range(0f, 0.5f), 0);
-            mineral.GetComponent<Rigidbody2D>().velocity = rigidbody2d.velocity + new Vector2(Random.Range(-0.5f, 0f), Random.Range(-0.5f, 0.5f));
+            mineral.GetComponent<Rigidbody2D>().velocity = rb.velocity + new Vector2(Random.Range(-0.5f, 0f), Random.Range(-0.5f, 0.5f));
+        }
+
+        foreach (var a in SubAsteroids)
+        {
+            var asteroid = Instantiate(a);
+            asteroid.position = transform.position + new Vector3(Random.Range(0f, 0.5f), Random.Range(0f, 0.5f), 0);
+            asteroid.GetComponent<Rigidbody2D>().velocity = rb.velocity + new Vector2(Random.Range(-0.5f, 0f), Random.Range(-0.5f, 0.5f));
         }
 
         Destroy(gameObject);
