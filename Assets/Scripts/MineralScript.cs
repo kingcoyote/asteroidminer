afca7b6d14d3a7f93b5c22384c1be923f46928c3
Steve Phillips <kingcoyote85@gmail.com>
2015-11-14 20:14:27 -0800

    cleaned up the minerals and added particle effects

--- Assets/Scripts/MineralScript.cs
+++ Assets/Scripts/MineralScript.cs
@@ -1,17 +1,6 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class MineralScript : MonoBehaviour
 {
     public int Value = 5;
-
-    // Use this for initialization
-    public void Start () {
-    
-    }
-    
-    // Update is called once per frame
-    public void Update () {
-    
-    }
 }
