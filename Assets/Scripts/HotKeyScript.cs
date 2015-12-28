bb6de51bfc45e28ac18273cbfc4f23d9866ffb10
Steve Phillips <kingcoyote85@gmail.com>
2015-12-28 09:47:07 -0800

    added hotkeys to the upgrade buttons with text labels

---
+++ Assets/Scripts/HotKeyScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class HotKeyScript : MonoBehaviour
+{
+    public KeyCode Key;
+
+    private Button _button;
+
+	// Use this for initialization
+	public void Start ()
+	{
+	    _button = gameObject.GetComponent<Button>();
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+	    if (Input.GetKeyDown(Key)) _button.onClick.Invoke();
+	}
+}
