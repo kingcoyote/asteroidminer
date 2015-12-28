c7be90645838d6837560a9bad3eee26813fb2866
Steve Phillips <kingcoyote85@gmail.com>
2015-12-28 11:53:16 -0800

    added script to correct the position of the player on launch to account for different resolutions

---
+++ Assets/Scripts/PlayerCameraScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class PlayerCameraScript : MonoBehaviour
+{
+    public void Start()
+    {
+        var collider = gameObject.GetComponent<Collider2D>();
+        transform.position = new Vector3(Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0)).x + 1, transform.position.y, transform.position.z);
+    }
+
+	public void Update () {
+        if (Application.isEditor)
+        {
+            Start();
+        }
+	}
+}
