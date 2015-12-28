c5c42f97dc7c4ab0083e11c751ad244135b42f9e
Steve Phillips <kingcoyote85@gmail.com>
2015-12-28 09:32:39 -0800

    added a red disabled color to upgrade buttons if they can't be purchased at the moment

--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -15,12 +15,24 @@ public class UIScript : MonoBehaviour
     private Text _weaponUpgrade;
     private Text _miningUpgrade;
 
+    private Button _engineUpgradeButton;
+    private Button _healthUpgradeButton;
+    private Button _shieldUpgradeButton;
+    private Button _weaponUpgradeButton;
+    private Button _miningUpgradeButton;
+
     // Use this for initialization
     public void Start ()
     {
         _health = gameObject.transform.Find("StatusPanel/HealthSlider").GetComponent<Slider>();
         _shield = gameObject.transform.Find("StatusPanel/ShieldSlider").GetComponent<Slider>();
         _money = gameObject.transform.Find("StatusPanel/MoneyText").GetComponent<Text>();
+
+        _engineUpgradeButton = gameObject.transform.Find("UpgradePanel/EngineUpgrade").GetComponent<Button>();
+        _healthUpgradeButton = gameObject.transform.Find("UpgradePanel/HealthUpgrade").GetComponent<Button>();
+        _shieldUpgradeButton = gameObject.transform.Find("UpgradePanel/ShieldUpgrade").GetComponent<Button>();
+        _weaponUpgradeButton = gameObject.transform.Find("UpgradePanel/WeaponUpgrade").GetComponent<Button>();
+        _miningUpgradeButton = gameObject.transform.Find("UpgradePanel/MiningUpgrade").GetComponent<Button>();
 
         _engineUpgrade = gameObject.transform.Find("UpgradePanel/EngineUpgrade").GetComponentInChildren<Text>();
         _healthUpgrade = gameObject.transform.Find("UpgradePanel/HealthUpgrade").GetComponentInChildren<Text>();
@@ -35,6 +47,12 @@ public class UIScript : MonoBehaviour
         _health.value = Player.HealthPercent * 100;
         _shield.value = Player.ShieldPercent * 100;
         _money.text = string.Format("Minerals\n{0}", Player.Money);
+
+        _engineUpgradeButton.interactable = Player.Money > PlayerScript.CostOfUpgrade(Player.EngineLevel);
+        _healthUpgradeButton.interactable = Player.Money > PlayerScript.CostOfUpgrade(Player.HealthLevel);
+        _shieldUpgradeButton.interactable = Player.Money > PlayerScript.CostOfUpgrade(Player.ShieldLevel);
+        _weaponUpgradeButton.interactable = Player.Money > PlayerScript.CostOfUpgrade(Player.WeaponLevel);
+        _miningUpgradeButton.interactable = Player.Money > PlayerScript.CostOfUpgrade(Player.MiningLevel);
 
         _engineUpgrade.text = string.Format("Engines\n({0})", PlayerScript.CostOfUpgrade(Player.EngineLevel));
         _healthUpgrade.text = string.Format("Health\n({0})", PlayerScript.CostOfUpgrade(Player.HealthLevel));
